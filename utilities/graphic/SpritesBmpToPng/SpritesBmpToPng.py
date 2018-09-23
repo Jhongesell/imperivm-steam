9d2357a44f4a770538ec2442964ebab24b32df1b
fabian <fabio.marigo@outlook.it>
2018-09-23 21:38:55 +0200

    bmptopng

--- utilities/graphic/SpritesBmpToPng/SpritesBmpToPng.py
+++ utilities/graphic/SpritesBmpToPng/SpritesBmpToPng.py
@@ -3,6 +3,7 @@
 from PIL import Image
 import numpy as np
 import os
+from SpritesFunctions import SpritesFixer
 
 dir_path = os.getcwd()
 
@@ -22,42 +23,10 @@ if N_bmp > 0:
             name = BMP[:-4]
             
             im = Image.open('bmp\\'+BMP)
-            im = im.convert('RGBA')
-
-            data = np.array(im)
-            red, green, blue, alpha = data.T
             
-            back = list(data[0][0])
+            im_out = SpritesFixer(im)
             
-            back_areas = (red == back[0]) & (blue == back[2]) & (green == back[1])
-            
-            data[..., :][back_areas.T] = (255, 255, 255, 0)
-
-            positions_y = []
-            i=0
-            
-            for el in data[0]:
-                
-                if el[3] != 0:
-                    positions_y.append(i)
-                i+=1
-                
-            positions_x = []
-            i=0
-            for el in data:    
-                if el[0][3] != 0:
-                    positions_x.append(i)        
-                i+=1
-                
-            for i in positions_x:
-                data[i] = (255,255,255,0)
-                  
-            for n in range(data.shape[0]):
-                for j in positions_y:
-                    data[n][j]=(255,255,255,0)
-                    
-            im2 = Image.fromarray(data)
-            im2.save("png\\"+name+".png", "PNG")
+            im_out.save("png\\"+name+".png", "PNG")
             
             print(name+' OK')
             
---
+++ utilities/graphic/SpritesBmpToPng/SpritesFunctions.py
@@ -0,0 +1,96 @@
+from PIL import Image
+from collections import Counter
+import numpy as np
+
+
+def SpritesFixer(pil_im):
+
+    IMG = np.asarray(pil_im.convert('RGBA'))
+    IMG_data = list(pil_im.getdata())
+
+    # BASIC INFORMATIONS
+
+    WIDTH = IMG.shape[1]
+    HEIGHT = IMG.shape[0]
+    DIRECTIONS = Counter(IMG_data[:WIDTH]).most_common()[1][1]+1
+    FRAMES = Counter(list(map(tuple,IMG[:,:1].reshape(1,HEIGHT,4)[0]))).most_common()[1][1]+1
+    CELL_WIDTH = int((WIDTH-DIRECTIONS+1)/DIRECTIONS)
+    CELL_HEIGHT = int((HEIGHT-FRAMES+1)/FRAMES)
+
+    # CALCOL0 AREA DI RITAGLIO
+
+    pil_back = Image.new('RGBA', (CELL_WIDTH,CELL_HEIGHT), (255, 255, 255, 0))
+    back = np.asarray(pil_back)
+    back.setflags(write=1)
+
+    D = {}
+
+    for i in range(DIRECTIONS):
+        for j in range(FRAMES):
+            img = IMG[(j*CELL_HEIGHT+j):(j*CELL_HEIGHT+CELL_HEIGHT+j) , (i*CELL_WIDTH+i):(i*CELL_WIDTH+CELL_WIDTH+i)]
+            img.setflags(write=1)       
+            red, green, blue, alpha = img.T
+            back = list(img[0][0])
+            back_areas = (red == back[0]) & (blue == back[2]) & (green == back[1])
+            img[..., :][back_areas.T] = (255, 255, 255, 0)
+            D[(j,i)]=img
+            pil_img = Image.fromarray(img)
+            pil_back.paste(pil_img, (0, 0), pil_img)
+
+    pasted0 = np.asarray(pil_back)  
+    pasted1 = np.rot90(pasted0, 1)
+    pasted2 = np.rot90(pasted0, 2)
+    pasted3 = np.rot90(pasted0, 3)
+
+    h0 = 0
+    for row in pasted0:
+        if sum(row)[3] == 0:
+            h0+=1
+        else: 
+            break
+
+    h1 = 0
+    for row in pasted2:
+        if sum(row)[3] == 0:
+            h1+=1
+        else: 
+            break
+
+    h1 = CELL_HEIGHT-h1        
+
+    w0 = 0
+    for row in pasted3:
+        if sum(row)[3] == 0:
+            w0+=1     
+        else: 
+            break
+
+    w1 = 0
+    for row in pasted1:
+        if sum(row)[3] == 0:
+            w1+=1
+        else: 
+            break
+    w1 = CELL_WIDTH-w1       
+
+    h = h1-h0
+    w = w1-w0
+
+    # RITAGLIO SPRITES
+
+    for (k,v) in D.items():
+        D[k]=v[h0:h1,w0:w1]
+
+    # CREAZIONE SPRITE FINALE
+    
+    W = w * FRAMES
+    H = h * DIRECTIONS
+
+    BACK = np.asarray(Image.new('RGBA', (W,H), (255, 255, 255, 0)))
+    BACK.setflags(write=1)        
+
+    for i in range(DIRECTIONS):
+        for j in range(FRAMES):
+            BACK[i*h:(i*h+h), j*w:(j*w+w)] = D[(j,i)]
+
+    return Image.fromarray(BACK)
